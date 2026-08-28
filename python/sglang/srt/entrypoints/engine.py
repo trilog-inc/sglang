ac97c9cc9d3b1dc68072b65f832de4bf45f34910
myfrienderic <hi@myfrienderic.com>
2026-08-28 11:35:12 -0400

    kda transpose fix

--- python/sglang/srt/entrypoints/engine.py
+++ python/sglang/srt/entrypoints/engine.py
@@ -870,7 +870,7 @@ def _set_envs_and_config(server_args: ServerArgs):
         if server_args.attention_backend == "flashinfer":
             assert_pkg_version(
                 "flashinfer_python",
-                "0.6.3",
+                "0.6.17",
                 "Please uninstall the old version and "
                 "reinstall the latest version by following the instructions "
                 "at https://docs.flashinfer.ai/installation.html.",
